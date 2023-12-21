65ab2018ec3bcb5823926d5869d494acd954c175
vikash sai <vikashsai@vikashs-MacBook-Pro.local>
2023-12-22 00:21:37 +0530

    first commit.

---
+++ src/main/java/com/vikash/blog/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.vikash.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+
+		System.out.println("hi vikash , welcome to jp morgan & chase!. ");
+	}
+
+}
